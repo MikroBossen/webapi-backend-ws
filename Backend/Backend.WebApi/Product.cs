49f977ca9d75dfc2ae2c9c23eaacdeff9ee38729
Berit Østermark <bos@DSVM.DK>
2016-09-13 16:42:34 +0200

    Hjemme 13-09-2016

---
+++ Backend/Backend.WebApi/Product.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Backend.WebApi
+{
+    public class Product
+    {
+        private int id;
+        private string name;
+        private string category;
+        private decimal price;
+
+        public Product(int Id, string Name, string Category, decimal Price)
+        {
+            this.id = Id;
+            this.name = Name;
+            this.category = Category;
+            this.price = Price;
+        }
+
+        public int Id
+        {
+            get
+            {
+                return this.id;
+            }
+            set
+            {
+                this.id = value;
+            }
+        }
+
+        public string Name
+        {
+            get
+            {
+                return this.name;
+            }
+            set
+            {
+                this.name = value;
+            }
+        }
+
+        public string Category
+        {
+            get
+            {
+                return this.category;
+            }
+            set
+            {
+                this.category = value;
+            }
+        }
+
+        public decimal Price
+        {
+            get
+            {
+                return this.price;
+            }
+            set
+            {
+                this.price = value;
+            }
+        }
+    }
+}
---
+++ Backend/Backend.WebApi/ProductsController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Backend.WebApi
+{
+    public class ProductsController : ApiController
+    {
+        private Product[] products = new Product[5] { new Product(101, "Handbag", "Accessories", 12.50m),
+                                                      new Product(207, "Shirt", "Clothes", 5.25m),
+                                                      new Product(315, "Leather boots", "Shoes", 25.00m),
+                                                      new Product(108, "Necklace", "Accessories", 51.50m),
+                                                      new Product(202, "Shorts", "Clothes", 7.95m) };
+
+        public IEnumerable<Product> GetAllProducts()
+        {
+            return this.products;
+        }
+
+        public IHttpActionResult GetProduct(int id)
+        {
+            for (int i = 0; i < products.Length; i++)
+            {
+                if (products[i].Id == id)
+                {
+                    return Ok(products[i]);
+                }
+            }
+            return NotFound();
+        }
+    }
+}
