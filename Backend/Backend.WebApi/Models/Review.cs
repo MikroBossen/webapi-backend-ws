58a5b227560923098697eb12530c4cb4fe44433b
Admin <Admin>
2016-09-14 15:00:25 +0200

    Kursus 14-09-2016

---
+++ Backend/Backend.WebApi/Models/Review.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Backend.WebApi.Models
+{
+    public class Review
+    {
+        private int id;
+        private int productId;
+        private int rating;
+        private string text;
+
+        public int Id
+        {
+            get
+            {
+                return this.id;
+            }
+            set
+            {
+                this.id = value;
+            }
+        }
+
+        public int ProductId
+        {
+            get
+            {
+                return this.productId;
+            }
+            set
+            {
+                this.productId = value;
+            }
+        }
+
+        public int Rating
+        {
+            get
+            {
+                return this.rating;
+            }
+            set
+            {
+                this.rating = value;
+            }
+        }
+
+        public string Text
+        {
+            get
+            {
+                return this.text;
+            }
+            set
+            {
+                this.text = value;
+            }
+        }
+    }
+}
--- Backend/Backend.WebApi/ProductsController.cs
+++ Backend/Backend.WebApi/ProductsController.cs
@@ -9,11 +9,11 @@ namespace Backend.WebApi
 {
     public class ProductsController : ApiController
     {
-        private Product[] products = new Product[5] { new Product(101, "Handbag", "Accessories", 12.50m),
-                                                      new Product(207, "Shirt", "Clothes", 5.25m),
-                                                      new Product(315, "Leather boots", "Shoes", 25.00m),
-                                                      new Product(108, "Necklace", "Accessories", 51.50m),
-                                                      new Product(202, "Shorts", "Clothes", 7.95m) };
+        private Product[] products = new Product[] { new Product(101, "Handbag", "Accessories", 12.50m),
+                                                     new Product(207, "Shirt", "Clothes", 5.25m),
+                                                     new Product(315, "Leather boots", "Shoes", 25.00m),
+                                                     new Product(108, "Necklace", "Accessories", 51.50m),
+                                                     new Product(202, "Shorts", "Clothes", 7.95m) };
 
         public IEnumerable<Product> GetAllProducts()
         {
